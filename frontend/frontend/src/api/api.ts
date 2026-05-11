6ff71a37323229b4ac0208aa06f138fb02687505
gurbhej23 <2000gurbhejsingh2311@gmail.com>
2026-05-11 11:11:57 +0530

    change something

--- frontend/frontend/src/api/api.ts
+++ frontend/frontend/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    import.meta.env.VITE_API_BASE_URL || "https://faceauth-ah1n.onrender.com/api/",
+    import.meta.env.VITE_API_BASE_URL,
 });
 
 export default api;
