da402563e33794920e16042c8a89e3375ad8759d
gurbhej23 <2000gurbhejsingh2311@gmail.com>
2026-05-11 11:50:49 +0530

    fix cors

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -51,6 +51,7 @@ if RENDER_EXTERNAL_HOSTNAME and RENDER_EXTERNAL_HOSTNAME not in ALLOWED_HOSTS:
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -58,7 +59,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "corsheaders",
     "accounts",
 ]
 
